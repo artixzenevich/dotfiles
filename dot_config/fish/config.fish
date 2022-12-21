e8faceab76f117a9be414b9c16ede873e13d3220
Artik Zenevich <azenevich91@gmail.com>
2022-12-21 19:24:08 +0300

    Init

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    alias v="nvim"
+    alias c="clear"
+    alias i="sudo dnf install"
+    alias off="poweroff"
+    alias r="ranger"
+end
+
+export EDITOR="/usr/bin/nvim"
